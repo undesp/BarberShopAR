a36b96bcfae0c9e3fe5184d5efd0657ed9c3f441
undesp <pavel.aksonov@gmail.com>
2018-06-19 10:44:57 +0800

    set database

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+require 'pg'
+
+set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'barbershopar', pool: 2, username: 'user', password: 'qwe'}
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
