6092f8703264f3e57c086509a4187fdc5f41d3fa
undesp <pavel.aksonov@gmail.com>
2018-06-19 10:47:45 +0800

    create entity Client

--- app.rb
+++ app.rb
@@ -7,6 +7,9 @@ require 'pg'
 
 set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'barbershopar', pool: 2, username: 'user', password: 'qwe'}
 
+# Сущность клиент
+class Client < ActiveRecord::Base
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
