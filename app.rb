62f779dc73622b92245cc1e68c391ad62fa80372
undesp <pavel.aksonov@gmail.com>
2018-06-19 11:10:55 +0800

    add rakeFile

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'barbersh
 
 # Сущность клиент
 class Client < ActiveRecord::Base
+	
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
