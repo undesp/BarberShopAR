9907e3f546faace9d33e5c9b2f009845ebd44890
undesp <pavel.aksonov@gmail.com>
2018-06-19 15:48:10 +0800

    create /visit, save post result to DB

--- app.rb
+++ app.rb
@@ -4,11 +4,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require 'pg'
+require 'time'
 
 set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'barbershopar', pool: 2, username: 'user', password: 'qwe'}
 
 # Сущность клиент
 class Client < ActiveRecord::Base
+	validates :name, presence: {message: 'Поле Имя не должно быть пустым' }
+	validates :phone, presence: {message: 'Поле Номер телефона не должно быть пустым' }
+	validates :datestamp, presence: {message: 'Поле Дата не должно быть пустым' }
 end
 class Barber < ActiveRecord::Base
 end
@@ -17,7 +21,6 @@ end
 
 helpers do
   def username
-
      session[:identity] ? session[:identity] : 'Login'
   end
 end
@@ -29,5 +32,21 @@ end
 
 get '/visit' do
 	@specs = Barber.order "name"
-erb :visit
+	erb :visit
+end
+
+post '/visit' do
+	@specs = Barber.order "name"
+	# Сохраняем результаты запроса в переменные
+	@Name = params[:inputName]
+	@Phone = params[:inputPhone]
+	@DateTime = params[:inputDateTime]
+	@Specialist = params[:inputSpecialist]
+	@Colorpicker = params[:colorpicker]
+	c = Client.new :name => @Name, :phone => @Phone, :datestamp => @DateTime, :barber => @Specialist, :color => @Colorpicker
+	@error = c.errors.messages
+	c.save
+	erb  :visit if c.new_record?
+	# redirect to '/visit'
+	erb	 "Вы записаны к #{@Specialist} на #{@DateTime} "
 end
