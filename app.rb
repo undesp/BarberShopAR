6d9f131847f34b12ef4b88961eb54aac9cc3ed17
undesp <pavel.aksonov@gmail.com>
2018-06-19 16:53:28 +0800

    visit: get params by params[:client]

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
-require 'pg'
+# require 'pg'
 require 'time'
 
 set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'barbershopar', pool: 2, username: 'user', password: 'qwe'}
@@ -49,10 +49,32 @@ post '/visit' do
 	@DateTime = params[:inputDateTime]
 	@Specialist = params[:inputSpecialist]
 	@Colorpicker = params[:colorpicker]
-	c = Client.new :name => @Name, :phone => @Phone, :datestamp => @DateTime, :barber => @Specialist, :color => @Colorpicker
-	@error = c.errors.messages
+	# c = Client.new :name => @Name, :phone => @Phone, :datestamp => @DateTime, :barber => @Specialist, :color => @Colorpicker
+	c = Client.new params[:client]
 	c.save
-	erb  :visit if c.new_record?
-	# redirect to '/visit'
-	erb	 "Вы записаны к #{@Specialist} на #{@DateTime} "
+	
+	if c.new_record? 
+		@error = c.errors.messages
+		erb  :visit 
+	else 
+		erb	 "Вы записаны к #{@Specialist} на #{@DateTime} "
+	end
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+
+post '/contacts' do
+	@feedback = params[:feedback]
+	c = Feedback.new :message => @feedback
+	c.save
+	if c.new_record? 
+		@error = c.errors.messages
+		erb  :contacts 
+	else 
+		erb "Спасибо за Ваш отзыв!"
+	end
+	
 end
